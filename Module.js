2deaa26bdf65efbdedd2b736991c2c9dfb6aaf36
Naitort <Naitora00@gmail.com>
2024-03-02 01:25:37 +0300

    Тёмной тёмной ночью. Катя правит код

--- Module.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs').promises
-const view = reqiure('View.js')
-const path = require('path')
-
-async function someFn(){
-    const test1 = await fs.readFile(path, 'utf-8').split
-}
-
-module.exports = 
